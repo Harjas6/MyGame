2ddd34f2a535d8e3fb409d3bc6e9e7dcb1213022
Harjas <hbansoot@students.cs.ubc.ca>
2023-06-07 18:06:26 -0700

    added insturctions

--- main.py
+++ main.py
@@ -16,6 +16,8 @@ class Game:
 
 # Handles all the events within the games
     def run(self):
+        self.level.run()
+        self.start_screen()
         while True:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
@@ -31,7 +33,7 @@ class Game:
 
     def play_again(self):
         while True:
-            self.draw_text('GAME OVER, Press any key to play again')
+            self.draw_text('GAME OVER, Press any key to play again', (255, 255, 255),(WIDTH, 50), (0,HEIGHT-50))
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()
@@ -39,17 +41,28 @@ class Game:
                 elif event.type == pygame.KEYDOWN:
                     return True
 
-    def draw_text(self, text):
-        text_holder = pygame.Surface((WIDTH, 50))
+    def draw_text(self, text, color, size, pos):
+        text_holder = pygame.Surface(size)
         my_font = pygame.font.SysFont('Helvetica', 30)
         my_font.set_bold(True)
-        text = my_font.render(text, False, (255, 255, 255))
+        text = my_font.render(text, False, color)
         text_holder.blit(text, (WIDTH / 3, 10,))
-        self.screen.blit(text_holder, (0, 0))
+        self.screen.blit(text_holder, pos)
         pygame.display.update()
 
-     def start_screen(self):
-         pass
+    def start_screen(self):
+        while True:
+            self.draw_text("Use W, A, S, D or the arrow keys to move", (255,255,255), (WIDTH,50), (0, 0))
+            self.draw_text("Dodge the spikes and orange orbs", (255,255,255), (WIDTH,50), (0, 50))
+            self.draw_text("Press any key to continue", (255,255,255), (WIDTH,50), (0, 100))
+
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+                elif event.type == pygame.KEYDOWN:
+                    return
+            pygame.display.update()
 
 if __name__ == '__main__':
     game = Game()
